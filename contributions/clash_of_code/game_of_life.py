67f60cc03836b53de2a51ce83f29e9250b6f30ec
Benoît Daene <benoitdaene@gmail.com>
2021-04-15 11:58:25 +0200

    Add CoC game_of_life.py.

---
+++ contributions/clash_of_code/game_of_life.py
@@ -0,0 +1,24 @@
+
+height, width = map(int, input().split())
+board = [input() for _ in range(height)]
+
+
+def next_state(row, col):
+    alive_neighbors_count = 0
+    for r in range(-1, 2):
+        for c in range(-1, 2):
+            if (r, c) == (0, 0):
+                continue
+            if not (0 <= row + r < height) or not (0 <= col + c < width):
+                continue
+            if board[row+r][col+c] == 'O':
+                alive_neighbors_count += 1
+    if board[row][col] == ' ' and alive_neighbors_count == 3:
+        return 'O'
+    elif board[row][col] == 'O' and (alive_neighbors_count < 2 or alive_neighbors_count > 3):
+        return ' '
+    else:
+        return board[row][col]
+
+
+print('\n'.join(''.join(next_state(row, col) for col in range(width)) for row in range(height)))
