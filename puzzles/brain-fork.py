df049e79298da1ff118281a6a65208c8e277f4b4
Benoît Daene <benoitdaene@gmail.com>
2022-05-05 16:46:39 +0200

    Add other actions.

--- puzzles/brain-fork.py
+++ puzzles/brain-fork.py
@@ -1,6 +1,7 @@
-
 # See https://www.codingame.com/ide/puzzle/brain-fork
 
+from __future__ import annotations
+import sys
 from dataclasses import dataclass
 from string import ascii_uppercase
 from collections import deque
@@ -10,11 +11,16 @@ LETTERS = len(ALPHABET)
 STONES = 30
 
 
+def debug(*args):
+    print(*args, file=sys.stderr, flush=True)
+
+
 @dataclass(frozen=True)
 class State:
     instructions: str = ''
     stones: tuple[int, ...] = (0,) * STONES
     spelled_phrase: tuple[int, ...] = ()
+    previous_state: State = None
 
     def do_actions(self, actions: str):
         stones = deque(self.stones)
@@ -22,9 +28,9 @@ class State:
 
         for action in actions:
             if action == '>':
-                stones.rotate(1)
-            elif action == '<':
                 stones.rotate(-1)
+            elif action == '<':
+                stones.rotate(1)
             elif action == '+':
                 stones[0] = (stones[0] + 1) % LETTERS
             elif action == '-':
@@ -34,16 +40,28 @@ class State:
             else:
                 raise ValueError(f"Invalid action {action}")
 
-        return State(self.instructions + actions, tuple(stones), self.spelled_phrase + letters)
+        return State(self.instructions + actions, tuple(stones), self.spelled_phrase + letters, self)
 
     def cost(self):
         return len(self.instructions)
 
     def gen_actions(self, letter: int):
-        yield '+' * ((letter - self.stones[0]) % LETTERS) + '.'
+        for i, stone in enumerate(self.stones):
+            actions = ''
+            if i < STONES - i:
+                actions += '>' * i
+            else:
+                actions += '<' * (STONES - i)
+            j = (letter - stone) % LETTERS
+            if j < LETTERS - j:
+                actions += '+' * j
+            else:
+                actions += '-' * (LETTERS - j)
+            actions += '.'
+            yield actions
 
 
-STATES = 100
+STATES = 1
 
 
 def main():
@@ -57,7 +75,9 @@ def main():
                 new_states.add(state.do_actions(actions))
         states = sorted(new_states, key=State.cost)[:STATES]
 
-    print(min(states, key=State.cost).instructions)
+    best_state = min(states, key=State.cost)
+    print(best_state.instructions)
+    debug(best_state.cost())
 
 
 if __name__ == "__main__":
