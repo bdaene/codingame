230e8e7b2838ab294eb94e41376c574bd8a60c43
Benoît Daene <benoitdaene@gmail.com>
2022-05-12 16:21:38 +0200

    Improve staff detection.

--- puzzles/music-scores.py
+++ puzzles/music-scores.py
@@ -13,44 +13,63 @@ def load_image():
 
     data = input().split()
     for c, l in zip(data[::2], map(int, data[1::2])):
-        image_flat[pos:pos+l] = c == 'B'
+        image_flat[pos:pos + l] = c == 'B'
         pos += l
 
     return image
 
 
 def read_notes(image):
-    # Find staff
-    position = 0
-    while sum(image[:, position]) == 0:
-        position += 1
-    staff = image[:, position]
+    staff, staff_start, staff_step = read_staff(image)
+
+    def is_staff(column):
+        return not (column & ~staff).any()
 
     # split image by note
     notes = []
+    position = 0
     while position < image.shape[1]:
-        while position < image.shape[1] and (image[:, position] == staff).sum() == image.shape[0]:
+        while position < image.shape[1] and is_staff(image[:, position]):
             position += 1
         end = position
-        while end < image.shape[1] and (image[:, end] == staff).sum() < image.shape[0]:
+        while end < image.shape[1] and not is_staff(image[:, end]):
             end += 1
         notes.append(image[:, position:end])
         position = end
     notes = notes[:-1]  # remove blank after partition
 
-    return [read_note(staff, note) for note in notes]
+    return [read_note(note, staff, staff_start, staff_step) for note in notes]
 
 
-def read_note(staff, image):
+def read_staff(image):
+    # Find staff
+    position = 0
+    while not image[:, position].any():
+        position += 1
+    staff = image[:, position]
+
+    start = numpy.min(staff.nonzero())
+    size = numpy.min((~staff[start:]).nonzero())
+    stop = numpy.max(staff.nonzero()) + 1 - size
+    step = (stop - start) // 4
+
+    full_staff = staff.copy()
+    for i in range(1, full_staff.shape[0] // step + 1):
+        full_staff[i * step:] |= staff[:-i * step]
+        full_staff[:-i * step] |= staff[i * step:]
+
+    return full_staff, stop + size / 2 + step, -step / 2
+
+
+def read_note(image, staff, staff_start, staff_step):
     tail_limit = image.sum(axis=0).mean() * 1.4
     image_no_tail = image[:, image.sum(axis=0) < tail_limit]  # remove tail
-    mass = image_no_tail.sum(axis=1) - staff*image_no_tail.shape[1]
+    mass = image_no_tail.sum(axis=1) * ~staff
 
-    note_type = 'HQ'[bool(mass.sum() > (image.shape[1]/2)**2*numpy.pi*0.2)]
+    note_type = 'HQ'[bool(mass.sum() > (image.shape[1] / 2) ** 2 * numpy.pi * 0.2)]
 
     center_of_mass = (mass * numpy.arange(image.shape[0])).sum() / mass.sum()
-    staff_start, staff_end = numpy.max(staff.nonzero()), numpy.min(staff.nonzero())
-    note_height = (center_of_mass-staff_start)/(staff_end-staff_start) * 8 + 2
+    note_height = (center_of_mass - staff_start) / staff_step
 
     return 'CDEFGABCDEFG'[round(note_height)] + note_type
 
